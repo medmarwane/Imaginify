6e818c60045c41872b81fce0de5c8e64f0dcc4fe
Marwane <medmarwane@users.noreply.github.com>
2024-04-19 11:08:21 +0200

    Let download image

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,14 +1,26 @@
 "use client"
 
-import { dataUrl, debounce, getImageSize } from "@/lib/utils"
-import { CldImage } from "next-cloudinary"
+import { dataUrl, debounce, download, getImageSize } from "@/lib/utils"
+import { CldImage, getCldImageUrl } from "next-cloudinary"
 import { PlaceholderValue } from "next/dist/shared/lib/get-img-props"
 import Image from "next/image"
 import React from "react"
 import { Button } from "../ui/button"
 
 const TransformedImage = ({ image, type, title, isTransforming, setIsTransforming, transformationConfig, hasDownload = false }: TransformedImageProps) => {
-  const downloadHandler = () => {}
+  const downloadHandler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+
+    download(
+      getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig
+      }),
+      title
+    )
+  }
 
   return (
     <div className="flex flex-col gap-4">
